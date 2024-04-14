c0dc8c2c99e7c4322c6394a01cae328be0228d45
tarak1006 <kandula.tarakaram@gmail.com>
2024-04-13 23:24:52 -0500

    projectPortfolio

---
+++ incResultEstimation.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color: bisque;">
+    <h1>Final Estimation for the Project.</h1>
+    <?php
+        include ("incEstimation.php");
+    ?>
+    <strong><a href= 'project_portfolio.php'>Back to project_portfolio page</a></strong>
+</body>
+</html>
--- project_portfolio.php
+++ project_portfolio.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project Portfolio</title>
-    <link rel="stylesheet" href="StyleSheet.css">
+    <link rel="stylesheet" href="portStyleSheet.css">
 </head>
 <body>
     <nav>
@@ -19,6 +19,79 @@
         <a href="event_calendar.php"> Event Calendar</a>
         <a href="feedback.php"> Feedback </a>
     </nav>
+    <h1>Project Portfolio of TechSphere_Nexus</h1>
+
+    <div class="container">
+        <h2>Recent Projects</h2>
+
+        <div class="project">
+            <img src="./images/image1.jpg" alt="Project 1">
+            <h2>Project 1</h2>
+            <p>Description of Project 1 goes here.</p>
+            <p><b>Technologies used:</b> </p>
+        </div>
+
+        <div class="project">
+            <img src="./images/image2.jpg" alt="Project 2">
+            <h2>Project 2</h2>
+            <p>Description of Project 2 goes here.</p>
+            <p><b>Technologies used:</b> </p>
+        </div>
+
+        <div class="project">
+            <img src="./images/image1.jpg" alt="Project 3">
+            <h2>Project 3</h2>
+            <p>Description of Project 3 goes here.</p>
+            <p><b>Technologies used:</b> </p>
+        </div>
+    </div>
+
+    <h2>Need an Estimate for your Project? </h2>
+    <form action="incResultEstimation.php" method="POST">
+        <p>
+            <label for="nameTextBox">Please enter your Project name</label>
+            <input type="text" name="nameTextBox" id="nameTextBox" >
+        </p>
+        <p>
+            <label for="projSelect">Select Type of Project</label>
+            <select name="projectSelect" id="projSelect">
+                <option value="webDesign">Web Design</option>
+                <option value="webDevelopment">Web Development</option>
+                <option value="mobileApp">Mobile Application</option>
+                <option value="graphicDesign">Graphic Design</option>
+            </select>
+        </p>
+        <p>Select the requirements for your Project</p>
+
+        <input type="checkbox" id="frontend" name="req[]" value="Frontend (HTML, CSS, JavaScript)">
+        <label for="frontend">Frontend (HTML, CSS, JavaScript)</label><br>
+        
+        <input type="checkbox" id="backend" name="req[]" value="Backend (PHP, Java, Python, Ruby)">
+        <label for="backend">Backend (PHP, Java, Python, Ruby)</label><br>
+        
+        <input type="checkbox" id="database" name="req[]" value="Databases (MySQL, MongoDB, PostgreSQL, Cassandra )">
+        <label for="database">Databases (MySQL, MongoDB, PostgreSQL, Cassandra )</label><br>
+        
+        <input type="checkbox" id="fullstack" name="req[]" value="FullStack [Frontend (HTML, CSS, JavaScript)+Backend (PHP, Java, Python, Ruby)]">
+        <label for="fullstack">FullStack (Frontend+Backend)</label><br>
+
+        <p>
+            <label> Enter the Duration in Number of Days for project completion:</label>
+            <input type="number" name="durationTextBox" min="10" max="50" required>
+        </p>
+
+        <p>
+            <label> Enter the $ per hour:</label>
+            <input type="number" name="priceTextBox" min="20" max="40" required>
+        </p>
+
+        <p>
+            <input type="submit" name="submitButton" value="Calculate Cost" id="cost">
+        </p>
+   
+    </form>
+
+
 
     
 </body>
