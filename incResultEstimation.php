d1a390ace88030aec3a4fa2c2ebc506a0d7d7d9f
tarak1006 <kandula.tarakaram@gmail.com>
2024-04-20 11:48:12 -0500

    about and portfolio page changes

--- incResultEstimation.php
+++ incResultEstimation.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body style="background-color: bisque;">
+<body style="background-color: lightcyan;">
     <h1>Final Estimation for the Project.</h1>
     <?php
         include ("incEstimation.php");
